202ebae766a82ce5f71ae200ea01fb6ea40579c8
Maria Luisa & Youssef Kholeif <pair+maluisar.yykholeif@devbootcamp.com>
2017-11-06 12:07:18 -0800

    Add decrease and increase functionality

--- src/App.js
+++ src/App.js
@@ -3,12 +3,32 @@ import Counter from './Counter';
 import './App.css';
 
 class App extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      count1: 0,
+      count2: 0,
+      count3: 0
+    }
+
+    this.increment = this.increment.bind(this)
+    this.decrement = this.decrement.bind(this)
+  }
+
+  increment(counter, multiplier) {
+    this.setState({ [counter]: this.state[counter] + multiplier })
+  }
+
+  decrement(counter, multiplier) {
+    this.setState({ [counter]: this.state[counter] - multiplier })
+  }
+
   render() {
     return (
       <div className="page-center-frame">
-        <Counter />
-        <Counter />
-        <Counter />
+        <Counter multiplier={1} increment={ () => this.increment("count1", 1)} decrement= { () => this.decrement("count1", 1)} count={this.state.count1}/>
+        <Counter multiplier={2} increment={ () => this.increment("count2", 2)} decrement= { () => this.decrement("count2", 2)} count={this.state.count2}/>
+        <Counter multiplier={3} increment={ () => this.increment("count3", 3)} decrement= { () => this.decrement("count3", 3)} count={this.state.count3}/>
       </div>
     );
   }
