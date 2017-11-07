2dd3710bac6e41ab15d3c6fb893d66b7efb8020b
Lorraine Hemenway & Shannon Nolan <pair+flash00.shannonnolan17@devbootcamp.com>
2017-11-06 16:38:37 -0800

    Implement feature to display count total.

--- src/App.js
+++ src/App.js
@@ -3,12 +3,61 @@ import Counter from './Counter';
 import './App.css';
 
 class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+        count1: 0,
+        count2: 0,
+        count3: 0,
+      }
+
+
+    this.increment = this.increment.bind(this)
+    this.decrement = this.decrement.bind(this)
+  }
+
+  increment(incrementValue, keyName) {
+    this.setState({ [keyName]: this.state[keyName] + incrementValue })
+  }
+
+  decrement(decrementValue, keyName){
+    this.setState({ [keyName]: this.state[keyName] - decrementValue })
+  }
+
+  calculateTotal() {
+    const stuff = Object.values(this.state)
+    console.log(stuff)
+    const totalCount = stuff.reduce((count, sum) =>
+      count + sum)
+    return totalCount
+  }
+
   render() {
     return (
       <div className="page-center-frame">
-        <Counter amount={1}/>
-        <Counter amount={2}/>
-        <Counter amount={3}/>
+        <Counter
+          amount={this.state.count1}
+          increment={() => this.increment(1, "count1")}
+          decrement={() => this.decrement(1, "count1")}
+        />
+
+        <Counter
+          amount={this.state.count2}
+          increment={() => this.increment(2, "count2")}
+          decrement={() => this.decrement(2, "count2")}
+        />
+
+        <Counter
+          amount={this.state.count3}
+          increment={() => this.increment(3, "count3")}
+          decrement={() => this.decrement(3, "count3")}
+        />
+
+        <span>
+          { this.calculateTotal() }
+        </span>
+
       </div>
     );
   }
--- src/Counter.js
+++ src/Counter.js
@@ -2,30 +2,12 @@ import React from 'react';
 import './Counter.css';
 
 class Counter extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      count: 0
-    };
-
-    this.increment = this.increment.bind(this)
-    this.decrement = this.decrement.bind(this)
-  }
-
-  increment() {
-    this.setState({ count: this.state.count + this.props.amount })
-  }
-
-  decrement(){
-    this.setState({ count: this.state.count - this.props.amount })
-  }
-
   render() {
     return (
       <div className="counter">
-        <button onClick={this.increment}>+</button>
-        <div className="count">{this.state.count}</div>
-        <button onClick={this.decrement}>-</button>
+        <button onClick={this.props.increment}>+</button>
+        <div className="count">{this.props.amount}</div>
+        <button onClick={this.props.decrement}>-</button>
       </div>
     );
   }
